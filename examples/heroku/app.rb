d64f268a7c859113d944c51761f0800a824b7599
Konstantin Kosmatov <key@kosmatov.ru>
2016-10-11 17:56:42 +0000

    Improve Heroku example

--- examples/heroku/app.rb
+++ examples/heroku/app.rb
@@ -4,13 +4,3 @@ require './app/worker'
 require './app/web'
 
 Sidekiq::Web.register Sidekiq::Benchmark::Sample
-
-REDIS_CONFIG = { namespace: :sample }
-
-Sidekiq.configure_server do |config|
-  config.redis = REDIS_CONFIG
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = REDIS_CONFIG
-end
--- examples/heroku/app/web.rb
+++ examples/heroku/app/web.rb
@@ -1,8 +1,14 @@
 module Sidekiq
   module Benchmark
     module Sample
+      WEB_DIR = File.expand_path("../../", __FILE__).freeze
+      VIEW_PATH = File.join(WEB_DIR, "views", "generate.erb").freeze
 
       def self.registered(app)
+        app.get "/benchmarks/generate" do
+          erb File.read VIEW_PATH
+        end
+
         app.post "/benchmarks/generate" do
           10.times do
             Worker.defer
